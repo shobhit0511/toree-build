42dbeedb3840647ad4fe44521f1b1f6b5b103d70
Shobhit Shukla <sshuklao@in.ibm.com>
2017-08-09 18:28:16 +0530

    building toree for spark-2.0.2

--- build.sh
+++ build.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# IBM Confidential
-# OCO Source Materials
-# 5725-Y50
-# (C) Copyright IBM Corp. 2015
-# The source code for this program is not published or
-# otherwise divested of its trade secrets, irrespective
-# of what has been deposited with the U.S. Copyright Office.
-
 set -e
 set -x
 
